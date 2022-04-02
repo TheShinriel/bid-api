9d1be99e74fbe32afaf110d95bce9eeb56275911
Shinriel <the.shinriel@gmail.com>
2022-04-02 18:18:38 +0200

    Prepare rest actions to validate from event or enhanced request for custom mutations

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,29 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express'
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World from app.ts!'))
+app.get('/', (req: Request, res: Response) =>
+	res.send('Hello World from app.ts!')
+)
 
-export default app;
+// TODO: Open auction
+app.get('/openAuction', (req: Request, res: Response) => {
+	return res.json({
+		openedAt: new Date(),
+	})
+})
+
+// TODO: Close auction after 1minute
+app.get('/closeAuction', (req: Request, res: Response) => {
+	return res.json({
+		closedAt: new Date(),
+		// TODO: Check if there is a winner
+	})
+})
+
+// TODO: Validate bid, that is greather than last bid price
+app.get('/validateBid', (req: Request, res: Response) => {
+	return true
+})
+
+export default app
