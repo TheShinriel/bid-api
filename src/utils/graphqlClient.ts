94059adba1c6c1bc1652c12fabaf92403775844a
Shinriel <the.shinriel@gmail.com>
2022-04-03 20:43:25 +0200

    Review URL and secret to contact graphql api

--- src/utils/graphqlClient.ts
+++ src/utils/graphqlClient.ts
@@ -1,19 +1,35 @@
 import fetch from 'node-fetch'
+import 'dotenv/config'
+
 import { HasuraGraphQlError } from 'src/model'
+import { exit } from 'process'
 
 export type GraphqlResponse = { data: any; errors: HasuraGraphQlError }
+
+const { HASURA_GRAPHQL_URL, HASURA_GRAPHQL_ADMIN_SECRET } = process.env
+if (!HASURA_GRAPHQL_URL) {
+	console.error('HASURA_GRAPHQL_URL is not defined')
+	exit(1)
+}
+if (!HASURA_GRAPHQL_ADMIN_SECRET) {
+	console.error('HASURA_GRAPHQL_URL is not defined')
+	exit(1)
+}
 
 // execute the parent mutation in Hasura
 export const executeGraphqlQuery = async (
 	variables: { [key: string]: any },
 	query: string
 ): Promise<GraphqlResponse> => {
-	const fetchResponse = await fetch('http://localhost:8080/v1/graphql', {
+	const fetchResponse = await fetch(HASURA_GRAPHQL_URL, {
 		method: 'POST',
 		body: JSON.stringify({
 			query,
 			variables,
 		}),
+		headers: {
+			'x-hasura-admin-secret': HASURA_GRAPHQL_ADMIN_SECRET,
+		},
 	})
 	return (await fetchResponse.json()) as GraphqlResponse
 }
